7e14c99f5469bdfed619205ad0da240af4ff97d4
proudcanadianeh <proudcanadianeh@gmail.com>
2018-01-23 08:51:53 -0800

    Corrected URL
    Changed URL's to HTTPS

--- jre8/master/tools/chocolateyInstall.ps1
+++ jre8/master/tools/chocolateyInstall.ps1
@@ -43,9 +43,9 @@ $arguments = @{}
   $packageName = 'jre8'
   # Modify these values -----------------------------------------------------
   # Find download URLs at http://www.java.com/en/download/manual.jsp
-  $url = 'http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230540_2f38c3b165be4555a1fa6e98c45e0808'
+  $url = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=230540_2f38c3b165be4555a1fa6e98c45e0808'
   $checksum32 = '9D4AB1C5EFF210FD165A1ACA33734DA82C6BAC9437BD8F20637E68460F3E9008'
-  $url64 = 'http://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_2f38c3b165be4555a1fa6e98c45e0808'
+  $url64 = 'https://javadl.oracle.com/webapps/download/AutoDL?BundleId=230542_2f38c3b165be4555a1fa6e98c45e0808'
   $checksum64 = '59C02EB80ACFAEEAF3728C3841440C68EA9E3B4D5E735A7A863F6E2492760B5D'
   $oldVersion = '8.0.1510.12'
   $version = '8.0.1610'
