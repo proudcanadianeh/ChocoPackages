73ede0ef4d235019ddf9684406f36b1be5953cca
proudcanadianeh <proudcanadianeh@gmail.com>
2018-07-20 16:01:39 -0700

    Finalization of 8.0.181
    Finished testing on win10, win 7 32+64

--- jre8/master/tools/chocolateyInstall.ps1
+++ jre8/master/tools/chocolateyInstall.ps1
@@ -37,8 +37,6 @@ $arguments = @{}
   }
 
   $scriptDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  # Import function to test if JRE in the same version is already installed. Legacy, phasing out. 
-  # Import-Module (Join-Path $scriptDir 'thisJreInstalled.ps1')
   
   $packageName = 'jre8'
   # Modify these values -----------------------------------------------------
@@ -59,9 +57,7 @@ $arguments = @{}
   Write-Output "Searching if new version exists..."
   $checkreg64 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object { $_.DisplayName -like '*Java 8*' -and ([Version]$_.DisplayVersion) -eq $version} -ErrorAction SilentlyContinue
   $checkreg32 = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion | Where-Object { $_.DisplayName -like '*Java 8*' -and ([Version]$_.DisplayVersion) -eq $version} -ErrorAction SilentlyContinue
-
-  #$thisJreInstalledHash = thisJreInstalled($version)
-
+  
 
     # Checks if JRE 32/64-bit in the same version is already installed and if the user excluded 32-bit Java.
     # Otherwise it downloads and installs it.
@@ -72,6 +68,7 @@ $arguments = @{}
     } 
     elseif ($exclude -ne "32") 
     {
+      Write-Output "Installing JRE $version 32-bit"
       Install-ChocolateyPackage $packageName $installerType $installArgs $url -checksum $checksum32 -checksumtype 'sha256'
     } 
     else 
@@ -89,6 +86,7 @@ $arguments = @{}
       } 
       elseif ($exclude -ne "64") 
       {
+        Write-Output "Installing JRE $version 64-bit"
         Install-ChocolateyPackage $packageName $installerType $installArgs64 -url64bit $url64 -checksum64 $checksum64 -checksumtype64 'sha256'
       } 
       else 
@@ -99,7 +97,6 @@ $arguments = @{}
   
   #Uninstalls the previous version of Java if either version exists
   Write-Output "Searching if the previous version exists..."
-  #$oldJreInstalledHash = thisJreInstalled($oldVersion)
   $checkoldreg64 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, PSChildName | Where-Object { $_.DisplayName -like '*Java 8*' -and ([Version]$_.DisplayVersion) -eq $oldversion} -ErrorAction SilentlyContinue
   $checkoldreg32 = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, PSChildName | Where-Object { $_.DisplayName -like '*Java 8*' -and ([Version]$_.DisplayVersion) -eq $oldversion} -ErrorAction SilentlyContinue
  
@@ -111,8 +108,8 @@ $arguments = @{}
   }
   if($checkoldreg64 -ne $null)
   {
-     Write-Warning "Uninstalling JRE version $oldVersion 64bit"
-     $64 = $checkoldreg64.PSChileName
+     Write-Warning "Uninstalling JRE version $oldVersion $osBitness bit" #Formatted weird because this is used if run on a x86 install
+     $64 = $checkoldreg64.PSChildName
      Start-ChocolateyProcessAsAdmin "/qn /norestart /X$64" -exeToRun "msiexec.exe" -validExitCodes @(0,1605,3010)
   }
 } catch {
--- jre8/master/tools/chocolateyUninstall.ps1
+++ jre8/master/tools/chocolateyUninstall.ps1
@@ -8,7 +8,7 @@ $version = '8.0.1810.13'
 #$thisJreInstalledHash = thisJreInstalled($version)
   $checkreg64 = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, PSChildName | Where-Object { $_.DisplayName -like '*Java 8*' -and ([Version]$_.DisplayVersion) -eq $version} -ErrorAction SilentlyContinue
   $checkreg32 = Get-ItemProperty HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, PSChildName | Where-Object { $_.DisplayName -like '*Java 8*' -and ([Version]$_.DisplayVersion) -eq $version} -ErrorAction SilentlyContinue
-
+  $osBitness = Get-ProcessorBits
 <#
 Exit Codes:
     0: Java installed successfully.
@@ -24,7 +24,7 @@ if($checkreg32 -ne $null)
   }
   if($checkreg64 -ne $null)
   {
-     Write-Warning "Uninstalling JRE version $Version 64bit"
+     Write-Warning "Uninstalling JRE version $Version $osBitness bit" #Formatted weird for x86 windows installs
      $64 = $checkreg64.PSChildName
      Start-ChocolateyProcessAsAdmin "/qn /norestart /X$64" -exeToRun "msiexec.exe" -validExitCodes @(0,1605,3010)
   }
